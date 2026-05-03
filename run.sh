224ad593b52845156ac2dad58249804345c9ef18
studyfranco <31127324+studyfranco@users.noreply.github.com>
2026-05-03 22:00:39 +0200

    log ?

--- run.sh
+++ run.sh
@@ -110,7 +110,7 @@ fi
 
 cd /config/gamefiles || exit 1
 
-exec ./WSServer.sh -server -log -SteamServerName="${SERVER_NAME}" -Port=${SERVER_PORT} -QueryPort=${SERVER_QUERY_PORT} -${GAME_MODE} -MaxPlayers=${MAXPLAYERS} ${LAUNCH_ARGS} -backup=${BACKUP} -saving=${SAVING} -online=Steam -forcepassthrough ${extra_opts[@]} &
+exec ./WSServer.sh -server -SteamServerName="${SERVER_NAME}" -Port=${SERVER_PORT} -QueryPort=${SERVER_QUERY_PORT} -${GAME_MODE} -MaxPlayers=${MAXPLAYERS} ${LAUNCH_ARGS} -backup=${BACKUP} -saving=${SAVING} -online=Steam -forcepassthrough ${extra_opts[@]} &
 
 # Capture Soulmask server start script pid
 init_pid=$!
